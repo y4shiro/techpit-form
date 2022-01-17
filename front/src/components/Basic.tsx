d4836b33ef1cad979e0a798de1c3fb0d8d82ab2b
y4shiro <y4shironao@gmail.com>
2022-01-17 12:01:40 +0900

    Basic コンポーネントの名前/自己紹介テキストフィールドを Redux で管理

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { TextField } from '@material-ui/core';
 
 import useStyles from './styles';
+import { RootState } from '../domain/entity/rootState';
+
+import { Profile } from '../domain/entity/profile';
+import profileActions from '../store/profile/actions';
 
 const Basic: React.FC = () => {
+  const dispatch = useDispatch();
+  const profile = useSelector((state: RootState) => state.profile);
   const classes = useStyles();
+
+  const handleChange = (member: Partial<Profile>) => {
+    dispatch(profileActions.setProfile(member));
+  };
 
   return (
     <>
-      <TextField fullWidth className={classes.formField} label="名前" />
+      <TextField
+        fullWidth
+        className={classes.formField}
+        label="名前"
+        value={profile.name}
+        onChange={(e) => handleChange({ name: e.target.value })}
+      />
       <TextField
         fullWidth
         multiline
         className={classes.formField}
         rows={5}
         label="自己紹介"
+        value={profile.description}
+        onChange={(e) => handleChange({ description: e.target.value })}
       />
     </>
   );
