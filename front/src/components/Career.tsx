b344d3eca545d330629e606b027153c20446283a
y4shiro <y4shironao@gmail.com>
2022-01-20 18:18:18 +0900

    職歴フォームのエラー後、要件を満たしたら再計算してエラー表示を消すよう実装

--- front/src/components/Career.tsx
+++ front/src/components/Career.tsx
@@ -14,17 +14,23 @@ import { RootState } from '../domain/entity/rootState';
 import { PROFILE } from '../domain/services/profile';
 import { exitEmptyCareers } from '../domain/services/career';
 
+import { calculateValidation } from '../domain/services/validation';
+import validationActions from '../store/validation/actions';
+
 import useStyles from './styles';
 
 const Career: React.FC = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const careers = useSelector((state: RootState) => state.profile.careers);
+  const profile = useSelector((state: RootState) => state.profile);
   const validation = useSelector((state: RootState) => state.validation);
   const isAbleToAddCareer = exitEmptyCareers(careers);
 
   const handleChange = (member: Partial<ICareer>, i: number) => {
     dispatch(profileActions.setCareer({ career: member, index: i }));
+
+    recalculateValidation(member, i);
   };
 
   const handleAddCareer = () => {
@@ -33,6 +39,20 @@ const Career: React.FC = () => {
 
   const handleDeleteCareer = (i: number) => {
     dispatch(profileActions.deleteCareer(i));
+  };
+
+  const recalculateValidation = (member: Partial<ICareer>, i: number) => {
+    if (!validation.isStartValidation) return;
+
+    const newProfile = {
+      ...profile,
+      career: profile.careers.map((c, _i) =>
+        _i === i ? { ...c, ...member } : c
+      ),
+    };
+
+    const message = calculateValidation(newProfile);
+    dispatch(validationActions.setValidation(message));
   };
 
   return (
